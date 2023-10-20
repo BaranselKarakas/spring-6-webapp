986a6ac112704fbfbb96e66ca7bd8256781794d7
Baransel Karakas <karakas@synyx.de>
2023-10-20 15:23:54 +0200

    24 completed24 completed24 completed24 completed24 completed24    completed24 completed24 completed24 completed24 completed24    completed24 completed24 completed24 completed24 completed24    completed24 completed24 completed24 completed24 completed24    completed24 completed24 completed

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
   private final AuthorRepository authorRepository;
   private final BookRepository bookRepository;
+  private final PublisherRepository publisherRepository;
 
-  public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+  public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
     this.authorRepository = authorRepository;
     this.bookRepository = bookRepository;
+    this.publisherRepository = publisherRepository;
   }
 
   @Override
@@ -44,12 +48,28 @@ public class BootstrapData implements CommandLineRunner {
 
     ericSaved.getBooks().add(dddSaved);
     rodSaved.getBooks().add(noEJBSSaved);
+    dddSaved.getAuthors().add(ericSaved);
+    noEJBSSaved.getAuthors().add(rodSaved);
+
+    Publisher publisher = new Publisher();
+    publisher.setPublisherName("My Publisher");
+    publisher.setAddress("123 Main");
+    Publisher savedPublisher = publisherRepository.save(publisher);
+
+    dddSaved.setPublisher(savedPublisher);
+    noEJB.setPublisher(savedPublisher);
 
     authorRepository.save(ericSaved);
     authorRepository.save(rodSaved);
+    bookRepository.save(dddSaved);
+    bookRepository.save(noEJBSSaved);
 
     System.out.println("In Bootstrap");
     System.out.println("Author Count: " + authorRepository.count());
     System.out.println("Book Count: " + bookRepository.count());
+
+
+
+    System.out.println("Publisher Count: " + publisherRepository.count());
   }
 }
--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6webapp.domain;
 
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
@@ -11,7 +12,6 @@ public class Book {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
-
   private String title;
   private String isbn;
 
@@ -21,6 +21,17 @@ public class Book {
       joinColumns = @JoinColumn(name = "book_id"),
       inverseJoinColumns = @JoinColumn(name = "author_id"))
   private Set<Author> authors = new HashSet<>();
+
+  @ManyToOne
+  private Publisher publisher;
+
+  public Publisher getPublisher() {
+    return publisher;
+  }
+
+  public void setPublisher(Publisher publisher) {
+    this.publisher = publisher;
+  }
 
   public Set<Author> getAuthors() {
     return authors;
